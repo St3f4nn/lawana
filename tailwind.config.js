dcacc4c4d6b94b84ecb2a4571b29e2ce009cb398
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-21 15:07:53 +0200

    Setting up the work environment - setting up the custom breakpoints, setting up the maximum widths of the container component for all screen sizes and setting up the general font family for a complete project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,48 @@ module.exports = {
     "./dist/*.html"
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px'
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      spacing: {
+        3.75: '15px'
+      },
+      fontFamily: {
+        'montserrat': "'Montserrat', sans-serif"
+      }
+    },
   },
   plugins: [
-    require('tailwindcss-debug-screens')
+    require('tailwindcss-debug-screens'),
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '670px'
+          },
+          '@screen md': {
+            maxWidth: '798px'
+          },
+          '@screen lg': {
+            maxWidth: '1054px'
+          },
+          '@screen xl': {
+            maxWidth: '1230px'
+          },
+          '@screen 2xl': {
+            maxWidth: '1230px'
+          }
+        }
+      })
+    }
   ],
 }
