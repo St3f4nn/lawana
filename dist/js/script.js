1d2d72f2f8e339cef076fd45d3fc155508be3705
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-26 22:37:44 +0200

    Optimizing code of the whole project...

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,24 @@
 'use strict';
 
+// Elements
+
 const navBurger = document.querySelector('#nav-burger');
 const navMenu = document.querySelector('#nav-menu');
 const htmlBody = document.querySelector('body');
+
+const showcaseSection = document.querySelector('#showcase');
+
+const showcaseBackgroundImages = [
+    'showcase_1',
+    'showcase_2',
+    'showcase_3'
+];
+
+const btnLeft = document.querySelector('#showcase-arrows-left');
+const btnRight = document.querySelector('#showcase-arrows-right');
+let counterNumber = 0;
+
+// Event listeners
 
 navBurger.addEventListener('click', () => {
     htmlBody.classList.toggle('overflow-hidden');
@@ -15,4 +31,24 @@ navBurger.addEventListener('click', () => {
     } else {
         navMenu.classList.toggle('w-4/5');
     };
+});
+
+btnLeft.addEventListener('click', () => {
+    counterNumber--;
+
+    if (counterNumber < 0) {
+        counterNumber = showcaseBackgroundImages.length - 1;
+    };
+
+    showcaseSection.style.backgroundImage = `url('./resources/images/02_showcase/${showcaseBackgroundImages[counterNumber]}.jpg')`;
+});
+
+btnRight.addEventListener('click', () => {
+    counterNumber++;
+
+    if (counterNumber > showcaseBackgroundImages.length - 1) {
+        counterNumber = 0;
+    };
+
+    showcaseSection.style.backgroundImage = `url('./resources/images/02_showcase/${showcaseBackgroundImages[counterNumber]}.jpg')`;
 });
